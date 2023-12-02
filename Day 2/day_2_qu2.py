456dbb319f895bf23b42788bf1d4b5753998df01
djn-hgs <daryl.noyce@highgateschool.org.uk>
2023-12-02 14:54:49 +0000

    Qu 1 done with a reasonably nice data structure

--- Day 2/day_2_qu2.py
+++ Day 2/day_2_qu2.py
@@ -1,17 +1,50 @@
 import re
 
-game_regex = re.compile('Game [0-9]+: (.*)')
+game_regex = re.compile('Game ([0-9]+): (.*)')
 
-red_regex = re.compile('([0-9]+) red')
-green_regex = re.compile('([0-9]+) green')
-blue_regex = re.compile('([0-9]+) blue')
+block_re = {
+    'red': re.compile('\s*([0-9]+) red'),
+    'green': re.compile('\s*([0-9]+) green'),
+    'blue': re.compile('\s*([0-9]+) blue')
+}
 
-with open('demo_qu1.txt', 'r') as file:
+game_sum = 0
+
+with open('input.txt', 'r') as file:
     for line in file.readlines():
-        [game] = game_regex.findall(line)
-        print(game)
+
+
+        [(game_num, game)] = game_regex.findall(line)
+
+        print(f'Game {game_num}: {game}')
 
         hands = game.split(';')
 
         print(hands)
 
+        possible = True
+
+        for hand in hands:
+            counters = {color: 0 for color in block_re}
+
+            blocks = hand.split(',')
+
+            for block in blocks:
+
+                for color in block_re:
+                    regex = block_re[color]
+
+                    if regex.match(block):
+                        [count] = regex.findall(block)
+                        print(f'{color} matches {count}')
+
+                        counters[color] += int(count)
+
+            if counters['red'] > 12 or counters['green'] > 13 or counters['blue'] > 14:
+                possible = False
+
+        if possible:
+            game_sum += int(game_num)
+
+
+print(game_sum)
