db1399831c4ebb1c084578099034fbb0bf005034
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-12-06 16:57:30 +0000

    Day 5 completed. Now onto Day 6

--- Day 5/day_5_qu2.py
+++ Day 5/day_5_qu2.py
@@ -1,11 +1,24 @@
 import re
+from dataclasses import dataclass
 
 seed_re = re.compile(r'seeds:\s(.*)')
 space_re = re.compile(r'\s*(\d+)\s+(\d+)\s*')
 map_re = re.compile(r'([a-z]+)-to-([a-z]+)\smap:')
 range_re = re.compile(r'(\d+)\s(\d+)\s(\d+)')
 
-range_dict = {}
+@dataclass
+class Frame:
+    start: int
+    stop: int
+    type: str
+
+@dataclass
+class Transform:
+    source_type: str
+    start: int
+    stop: int
+    target_type: str
+    target_start: int
 
 with open('input.txt', 'r') as file:
     first_line = file.readline()
@@ -13,9 +26,9 @@ with open('input.txt', 'r') as file:
     [seeds_list] = seed_re.findall(first_line)
     start_seeds_text = space_re.findall(seeds_list)
 
-    seed_dict = {int(v): int(r) for (v, r) in start_seeds_text}
+    source_frames = [Frame(start=int(v), stop =int(v) + int(r), type='seed') for (v, r) in start_seeds_text]
 
-    print(seed_dict)
+    transforms = []
 
     source = None
     target = None
@@ -25,38 +38,97 @@ with open('input.txt', 'r') as file:
 
             [(source, target)] = map_re.findall(line)
 
-            range_dict[source] = {target: {}}
-
         elif range_re.match(line):
             [(target_index, source_index, length)] = range_re.findall(line)
 
-            range_dict[source][target][int(source_index)] = (int(target_index), int(length))
+            transforms.append(
+                Transform(start=int(source_index),
+                          stop=int(source_index) + int(length),
+                          source_type=source,
+                          target_type=target,
+                          target_start=int(target_index)
+                          )
+            )
 
-print(range_dict)
+print(source_frames)
+print(transforms)
 
-for seed_index in start_seeds:
-    source = 'seed'
+chain = {
+    'seed': 'soil',
+    'soil': 'fertilizer',
+    'fertilizer': 'water',
+    'water': 'light',
+    'light': 'temperature',
+    'temperature': 'humidity',
+    'humidity': 'location'
+}
 
-    cursor = seed_index
+source = 'seed'
 
-    while source != 'location':
-        for target in range_dict[source]:
-            print(f'Source {source} Target {target} Cursor {cursor}')
-            for source_index in range_dict[source][target]:
-                target_index, length = range_dict[source][target][source_index]
+while source != 'location':
 
-                offset = cursor - source_index
+    target = chain[source]
 
-                if 0 <= offset < length:
-                    print(f'Adjusting {source_index} to {target_index} (length {length})')
-                    cursor = target_index + offset
-                    break
+    print(f'Source {source}, target {target}')
 
-            print(f'Adjusted cursor {cursor}')
+    target_frames = []
 
-        source = target
+    for transform in transforms:
+        if transform.source_type == source:
+            print(f'{transform}')
+            print(source_frames)
 
-    print(f'Seed {seed_index} ends up at location {cursor}\n')
-    start_seeds[seed_index] = cursor
+            spare_source_frames = []
 
-print(min(start_seeds.values()))
+            for frame in source_frames:
+                print(f'Frame {frame}')
+
+                if frame.start < transform.start <= frame.stop:
+                    left_frame = Frame(start=frame.start, stop=transform.start, type=source)
+
+                    spare_source_frames.append(left_frame)
+
+                    frame.start = transform.start
+
+                    print(f'Left {frame}')
+
+                if frame.start < transform.stop < frame.stop:
+                    right_frame = Frame(start=transform.stop, stop=frame.stop, type=source)
+
+                    spare_source_frames.append(right_frame)
+
+                    frame.stop = transform.stop
+
+                    print(f'Right {frame}')
+
+                if transform.start <= frame.start < frame.stop <= transform.stop:
+                    offset = transform.target_start - transform.start
+
+                    new_frame = Frame(
+                        start=frame.start + offset,
+                        stop=frame.stop + offset,
+                        type=target
+                    )
+
+                    target_frames.append(new_frame)
+
+                    print(f'Contained {frame} so mapped to {new_frame}')
+
+                else:
+                    spare_source_frames.append(frame)
+
+                    print(f'Spare {frame}')
+
+            source_frames = spare_source_frames
+
+    for frame in source_frames:
+        new_frame = Frame(start=frame.start, stop=frame.stop, type=target)
+
+        target_frames.append(new_frame)
+
+    source = target
+    source_frames = target_frames
+
+print(source_frames)
+
+print(min([frame.start for frame in source_frames]))
