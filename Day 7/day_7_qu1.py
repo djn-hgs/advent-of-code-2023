811ed0041feb26d5f6c39bb896972d75b4647472
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-12-07 21:26:09 +0000

    Day 1 and Day 2 trimmed down and sorted more carefully.

--- Day 7/day_7_qu1.py
+++ Day 7/day_7_qu1.py
@@ -1,86 +1,10 @@
-import builtins
-import types
-import typing
 from dataclasses import dataclass
 import re
 
 line_split = re.compile(r'(.+)\s(.+)')
 
-cards = '23456789TJQKA'
+CARDS = '23456789TJQKA'
 
-CARD_WEIGHT = {a: cards.index(a) + 1 for a in cards}
-
-@dataclass
-class Rule:
-    test: typing.Callable
-    label: str
-    weight: int
-
-
-def five(pattern: str):
-    return len(set(pattern)) == 1
-
-def four(pattern: str):
-    as_set = set(pattern)
-
-    if len(as_set) != 2:
-        return False
-
-    [a, b] = list(as_set)
-
-    if pattern.count(a) == 4 or pattern.count(b) == 4:
-        return True
-    else:
-        return False
-
-def full_house(pattern: str):
-    as_set = set(pattern)
-
-    if len(as_set) != 2:
-        return False
-
-    return 3 in [pattern.count(a) for a in as_set]
-
-def three(pattern: str):
-    as_set = set(pattern)
-
-    if len(as_set) != 3:
-        return False
-
-    return 3 in [pattern.count(a) for a in as_set]
-
-
-def two_pair(pattern: str):
-    as_set = set(pattern)
-
-    if len(as_set) != 3:
-        return False
-
-    return max([pattern.count(a) for a in as_set]) == 2
-
-
-def one_pair(pattern: str):
-    as_set = set(pattern)
-
-    if len(as_set) != 4:
-        return False
-
-    return max([pattern.count(a) for a in as_set]) == 2
-
-
-def high_card(pattern: str):
-    return len(set(pattern)) == 5
-
-RULES = [
-    Rule(test=five, label='5 of a kind', weight=7),
-    Rule(test=four, label='4 of a kind', weight=6),
-    Rule(test=full_house, label='Full house', weight=5),
-    Rule(test=three, label='Three of a kind', weight=4),
-    Rule(test=two_pair, label='Two pair', weight=3),
-    Rule(test=one_pair, label='Three of a kind', weight=2),
-    Rule(test=high_card, label='High card', weight=1)
-
-]
 
 @dataclass
 class Pattern:
@@ -88,27 +12,19 @@ class Pattern:
     value: int
 
     @property
-    def weight(self):
-        for rule in RULES:
-            if rule.test(self.text):
-                return rule.weight
+    def fingerprint(self):
+        letters = set(self.text)
+
+        letters_in_order = sorted(letters, key=self.text.count, reverse=True)
+
+        return [self.text.count(a) for a in letters_in_order]
+
+    def card_values(self):
+        return [CARDS.index(a) for a in self.text]
 
     def __lt__(self, other):
-        if self.weight < other.weight:
-            return True
-        elif self.weight == other.weight:
-            i = 0
-            while i < 5:
-                if CARD_WEIGHT[self.text[i]] < CARD_WEIGHT[other.text[i]]:
-                    return True
-                elif CARD_WEIGHT[self.text[i]] > CARD_WEIGHT[other.text[i]]:
-                    return False
+        return (self.fingerprint, self.card_values()) < (other.fingerprint, other.card_values())
 
-                i += 1
-            return False
-
-        else:
-            return False
 
 patterns = []
 
@@ -120,15 +36,9 @@ with open('input.txt', 'r') as file:
 
         patterns.append(Pattern(text=pattern, value=value))
 
-print(patterns)
-
 patterns.sort()
 
-total = 0
+score = sum([(i + 1) * pattern.value for i, pattern in enumerate(patterns)])
 
-for i, pattern in enumerate(patterns):
-    score = (i + 1) * pattern.value
-    print(f'{i + 1} for {pattern.text} with value {pattern.value} so score is {score}')
-    total += score
+print(score)
 
-print(total)
