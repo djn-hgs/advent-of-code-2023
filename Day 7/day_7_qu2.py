b3e605e4ffdefd69c3ff0d7a008d2dbb6e3e0862
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-12-07 21:19:04 +0000

    Day 7 trimmed down when I realised how I could use ordering more carefully.

--- Day 7/day_7_qu2.py
+++ Day 7/day_7_qu2.py
@@ -1,24 +1,9 @@
-import builtins
-import types
-import typing
 from dataclasses import dataclass
 import re
 
 line_split = re.compile(r'(.+)\s(.+)')
 
 CARDS = 'J23456789TQKA'
-
-CARD_WEIGHT = {a: CARDS.index(a) + 1 for a in CARDS}
-
-FINGERPRINT_WEIGHT = [
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 2],
-    [1, 2, 2],
-    [1, 1, 3],
-    [2, 3],
-    [1, 4],
-    [5]
-]
 
 
 @dataclass
@@ -27,10 +12,11 @@ class Pattern:
     value: int
 
     @property
-    def weight(self):
+    def fingerprint(self):
         letters = set(self.text)
 
-        letters_in_order = sorted(letters, key=self.text.count)
+        letters_in_order = sorted(letters, key=self.text.count, reverse=True)
+
         fingerprint = [self.text.count(a) for a in letters_in_order]
 
         j_count = self.text.count('J')
@@ -38,21 +24,20 @@ class Pattern:
         if 0 < j_count < 5:
 
             without = letters.difference({'J'})
-            without_in_order = sorted(without, key=self.text.count)
 
-            fingerprint_without = [self.text.count(a) for a in without_in_order]
+            without_in_order = sorted(without, key=self.text.count, reverse=True)
 
-            fingerprint_without[-1] += j_count
+            fingerprint = [self.text.count(a) for a in without_in_order]
 
-            return FINGERPRINT_WEIGHT.index(fingerprint_without)
-        else:
-            return FINGERPRINT_WEIGHT.index(fingerprint)
+            fingerprint[0] += j_count
+
+        return fingerprint
 
     def card_values(self):
         return [CARDS.index(a) for a in self.text]
 
     def __lt__(self, other):
-        return (self.weight, self.card_values()) < (other.weight, other.card_values())
+        return (self.fingerprint, self.card_values()) < (other.fingerprint, other.card_values())
 
 
 patterns = []
@@ -65,15 +50,9 @@ with open('input.txt', 'r') as file:
 
         patterns.append(Pattern(text=pattern, value=value))
 
-print(patterns)
-
 patterns.sort()
 
-total = 0
+score = sum([(i + 1) * pattern.value for i, pattern in enumerate(patterns)])
 
-for i, pattern in enumerate(patterns):
-    score = (i + 1) * pattern.value
-    print(f'{i + 1} for {pattern.text} with value {pattern.value} so score is {score}')
-    total += score
+print(score)
 
-print(total)
