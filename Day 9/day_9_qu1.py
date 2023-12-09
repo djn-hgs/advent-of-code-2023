50e48a78f1f195535c1a39560532e498ba7c1366
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-12-09 14:15:10 +0000

    Just remembered that `any` is a Python thing. Not sure I like it. Off to hit it with Haskell now.

--- Day 9/day_9_qu1.py
+++ Day 9/day_9_qu1.py
@@ -6,14 +6,15 @@ def next_value(last_line):
     else:
         return 0
 
-sum = 0
+
+my_sum = 0
 
 with open('input.txt', 'r') as file:
     for line in file.readlines():
         values = [int(v) for v in line.split(' ')]
 
-        sum += next_value(values)
+        my_sum += next_value(values)
 
         print(values, next_value(values))
 
-print(sum)
+print(my_sum)
