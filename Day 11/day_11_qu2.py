c5de2522d8912b45db5d75d2dbf72c209a003246
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-12-11 21:48:07 +0000

    Tidied things up a bit. Generator comprehension ftw

--- Day 11/day_11_qu2.py
+++ Day 11/day_11_qu2.py
@@ -1,45 +1,25 @@
 import math
 
-stars: [tuple[int, int]] = []
-used_rows: set[int] = set()
-used_columns: set[int] = set()
-
 # This was 2 in qu 1. It is the only change
 
 GAP_HEIGHT: int = 1000000
 
-height: int = 0
-width: int = 0
-
 with open('input.txt', 'r') as lines:
-    for row_num, line in enumerate(lines):
-        height += 1
-        width = max(width, len(line))
+    stars = [(col_num, row_num)
+             for row_num, line in enumerate(lines)
+             for col_num, symbol in enumerate(line)
+             if symbol == '#'
+             ]
 
-        for col_num, symbol in enumerate(line):
-            if symbol == '#':
-                used_rows.add(row_num)
-                used_columns.add(col_num)
-                stars.append((col_num, row_num))
+used_rows = {y for (_, y) in stars}
+used_columns = {x for (x, _) in stars}
+
+width = max(used_columns)
+height = max(used_rows)
 
 
-def raster_dist(x1: int, y1: int, x2: int, y2: int):
-    min_diff = min(abs(x2 - x1), abs(y2 - y1))
-    max_diff = max(abs(x2 - x1), abs(y2 - y1))
-
-    if min_diff == 0:
-        return max_diff
-
-    elif min_diff == 1:
-        return max_diff + 1
-
-    else:
-        step_length: int = math.floor(max_diff / (min_diff - 1))
-
-        additional: int = max_diff - (min_diff - 1) * step_length
-
-        return (step_length + 1) * (min_diff - 1) + additional + 1
-
+def taxi_dist(x1, y1, x2, y2):
+    return abs(x2 - x1) + abs(y2 - y1)
 
 def get_pos(x, y):
     return (
@@ -49,7 +29,7 @@ def get_pos(x, y):
 
 
 distance_sum = sum(
-    raster_dist(*get_pos(*s), *get_pos(*t))
+    taxi_dist(*get_pos(*s), *get_pos(*t))
     for s in stars
     for t in stars
     if s < t
